f2edb53509ab8e5e1e1e0e9ed8a74c71d555cd67
Andrej-oss <snowtomsawyer@gmail.com>
2020-11-08 16:22:00 +0100

    -finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {UserService} from './user/user.service';
+import {User} from './models/User';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-java';
+  form: FormGroup;
+  name: FormControl = new FormControl('');
+  users: User[];
+  constructor(private userService: UserService) {
+    this.form = new FormGroup({
+      name: this.name
+    });
+  }
+
+  save(form: FormGroup): void{
+    console.log(form.value.name);
+    this.userService.saveUser({name: `${form.value.name}`}).subscribe(user => this.users = user);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
